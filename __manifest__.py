ef1559cd9e4b13014ac2e4822a4ab2901098f252
cold-coder <abelmeles646@gmail.com>
2026-02-21 01:18:04 -0800

    feat: add first ui with action and menus

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv'
+        'views/estate_property_views.xml'
     ],
     'installable': True,
     'application': True,
