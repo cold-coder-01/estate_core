768440e59d981f2ca29afb332140f4240c2931ee
cold-coder <abelmeles646@gmail.com>
2026-02-21 00:49:30 -0800

    feat: complete estate.properties fields to match chap 3

---
+++ models/estate_properties.py
@@ -0,0 +1,26 @@
+from odoo import fields, models, api
+class EstateProperties(models.Model):
+    _name="estate.properties"
+    _description="Real Estate Property"
+   
+
+
+
+    # Basic Fields from Chapter 3
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.today())
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Orientation"
+    )
+    
