415dec758c0e0b54436417ed28494c7a45b6ca27
cold-coder <abelmeles646@gmail.com>
2026-02-21 00:23:11 -0800

    feat: flattened structure and added property model

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Real Estate',
+    'version':'1.0',
+    'category':'Real Esate/brokerage',
+    'summary':'manage property Advertisment',
+    'description': 'A module to manage real estate listings as per the Odoo 19.0 tutorial.',
+    'author':'Abel Meles',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    
+}
