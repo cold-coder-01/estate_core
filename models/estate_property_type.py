e1155ebdc4db3c8ad9b27c4ceea7530e79b74b72
cold-coder <abelmeles646@gmail.com>
2026-02-21 09:56:04 -0800

    feat: i added functionality

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -3,9 +3,10 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name="estate.property.type"
     _description="Property type"
-    _order="name"
+    _order="sequence, name"
 
     name= fields.Char(string="Name", required= True)
+    sequence = fields.Integer(string="Sequence", default=1, help="Used to order stages. Lower is better.")
     # Inline list of properties belonging to this type
     property_ids = fields.One2many("estate.properties", "property_type_id", string="Properties")
     _sql_constraints = [
